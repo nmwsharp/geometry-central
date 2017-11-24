f339d8383fb78cae0e73bbf6992b165a097abdab
Nick Sharp <nsharp@cs.cmu.edu>
2017-11-24 00:49:03 -0500

    Add glm and random fix

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,5 +1,9 @@
 #include <geometrycentral/utilities.h>
 
+namespace geometrycentral {
+
 std::random_device util_random_device;
-// std::mt19937 util_mersenne_twister( util_random_device() );
-std::mt19937 util_mersenne_twister(0);
+std::mt19937 util_mersenne_twister( util_random_device() );
+// std::mt19937 util_mersenne_twister(0); // uncomment for determinism
+
+}
