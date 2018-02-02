ba8ee729efe43cbec57bbd6cccf304bf528f78e4
Nick Sharp <nsharp@cs.cmu.edu>
2018-02-02 16:11:30 -0500

    resurrect FMM

--- include/geometrycentral/fast_marching_method.h
+++ include/geometrycentral/fast_marching_method.h
@@ -10,16 +10,12 @@
 // TODO: Split obtuse triangles instead of being wrong.
 namespace geometrycentral {
 
-VertexData<double> FMMDistance(
-    Geometry<Euclidean>* geometry,
-    const std::vector<std::pair<VertexPtr, double>>& initialDistances);
+VertexData<double> FMMDistance(Geometry<Euclidean>* geometry,
+                               const std::vector<std::pair<VertexPtr, double>>& initialDistances);
 
-VertexData<double> FMMDistance(
-    HalfedgeMesh* mesh,
-    const std::vector<std::pair<VertexPtr, double>>& initialDistances,
-    const EdgeData<double>& edgeLengths, const HalfedgeData<double>& oppAngles);
+VertexData<double> FMMDistance(HalfedgeMesh* mesh, const std::vector<std::pair<VertexPtr, double>>& initialDistances,
+                               const EdgeData<double>& edgeLengths, const HalfedgeData<double>& oppAngles);
 
-double eikonalDistanceSubroutine(double a, double b, double theta, double dA,
-                                 double dB);
+double eikonalDistanceSubroutine(double a, double b, double theta, double dA, double dB);
 
-}  // namespace geometrycentral
+} // namespace geometrycentral
