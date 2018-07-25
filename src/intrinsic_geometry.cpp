94781e00369c3d447716f4da353493bfb0305c47
Nick Sharp <nsharp@cs.cmu.edu>
2018-07-25 16:33:09 -0400

    duplicate files

---
+++ src/intrinsic_geometry.cpp
@@ -0,0 +1,30 @@
+#include "geometrycentral/geometry.h"
+#include <fstream>
+#include <limits>
+
+namespace geometrycentral {
+
+template <>
+void Geometry<Euclidean>::normalize() {
+  // compute center of mass
+  Vector3 cm;
+  for (VertexPtr v : mesh.vertices()) {
+    cm += position(v);
+  }
+  cm /= mesh.nVertices();
+
+  // translate to origin and determine radius
+  double rMax = 0;
+  for (VertexPtr v : mesh.vertices()) {
+    Vector3& p = position(v);
+    p -= cm;
+    rMax = std::max(rMax, norm(p));
+  }
+
+  // rescale to unit sphere
+  for (VertexPtr v : mesh.vertices()) {
+    position(v) /= rMax;
+  }
+}
+
+}  // namespace geometrycentral
